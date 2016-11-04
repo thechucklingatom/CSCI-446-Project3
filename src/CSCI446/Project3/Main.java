5c13bae049bc28aab43011b22eb25884166cfa07
thechucklingatom <thechucklingatom@gmail.com>
2016-11-04 15:56:55 -0600

    fixed some casting and modifier to remove warnings

--- src/CSCI446/Project3/Main.java
+++ src/CSCI446/Project3/Main.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class Main {
 
-	static final String CANCER = "data" + File.separator + "breast-cancer-wisconsin.data.txt";
+	private static final String CANCER = "data" + File.separator + "breast-cancer-wisconsin.data.txt";
 
 	public static void main(String[] args) {
 	// write your code here
 		DataContainer container = new DataContainer();
 		container.populateData(CANCER, 10);
 
-		List data = container.getData();
+		List<List<String>> data = container.getData();
 		List<String> classification = container.getClassification();
 
 		data.forEach(System.out::println);
