b88c4fa2c72ee604d568418902730d1261edaf0a
Robert Putnam <thechucklingatom@gmail.com>
2016-11-11 18:48:20 -0700

    added neighbor file with initial template, will work on it some more this weekend.

---
+++ src/CSCI446/Project3/kNearestNeighbor.java
@@ -0,0 +1,25 @@
+package CSCI446.Project3;
+
+import java.io.Writer;
+import java.util.List;
+
+/**
+ * Created by thechucklingatom on 11/11/2016.
+ *
+ * @author thechucklingatom
+ */
+public class kNearestNeighbor {
+	private Writer fileOutput;
+
+	kNearestNeighbor(Writer writer){
+		fileOutput = writer;
+	}
+
+	List<String> getNearestNeighbors(){
+		return null;
+	}
+
+	String getClassification(){
+		return null;
+	}
+}
