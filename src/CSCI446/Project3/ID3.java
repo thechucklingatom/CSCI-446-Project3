9ee76f3deb0d89d898075d73c3add6b3f9b7dff4
fraticellics <fraticellics@gmail.com>
2016-11-17 19:43:32 -0700

    jgk

--- src/CSCI446/Project3/ID3.java
+++ src/CSCI446/Project3/ID3.java
@@ -1,6 +1,8 @@
 package CSCI446.Project3;
 
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -11,20 +13,48 @@ public class ID3 {
     private Writer writer;
     private DataContainer container;
     private int currentFold;
+    private List<List<String>> trainingSet;
     private int testingFold;
     private Tree tree;
-
+    private List<List<String>> binAtt;
 
     public ID3(Writer inWriter, DataContainer inContainer){
         this.writer = inWriter;
         this.container = inContainer;
         tree = new Tree();
-
+        binAtt = new ArrayList<>();
     }
 
-    //if need be, this will go through the data and make descrete continuous data
-    public void makeDescrete(){
-        container.
+    //takes in a transposed training set (to make rows  attributes) and bin it
+    public void discretize(List<List<String>> inFold){
+        //iterationg through the attributes
+        for(int i = 0; i < inFold.size(); i++){
+            //we are going to determine the size of unique data of each attribute,
+            //and bin the attributes with a max of 5 unique data
+            int numBin = findNumBin(inFold.get(i));
+            bin(inFold.get(i), numBin);
+        }
+    }
+
+    public int findNumBin(List<String> inFold) {
+        int answer = 0;
+        HashSet noDupes = new HashSet();
+        for (int j = 0; j < inFold.size(); j++) {
+            noDupes.add(inFold.get(j));
+        }
+        if (noDupes.size() <= 5){
+            return answer;
+        } else {
+            return 6;
+        }
+    }
+
+    public void bin(List<String> inFold, int numBin){
+        if(numBin <= 5){} else{
+            for(int i = 0; i < inFold.size(); i++){
+
+            }
+        }
     }
 
     //this is the actual recursive method to run ID3
